d1b8bd4a3efa85b698abacd5288114fbcb326615
code-formula-ua <sliusarenko.channels@gmail.com>
2026-08-24 23:36:29 +0300

    Add project files.

---
+++ LiveClockApp/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+╔══════════════════╗
+║    LIVE CLOCK    ║
+╠══════════════════╣
+║     00:00:00     ║
+╚══════════════════╝
+*/
+
+Console.CursorVisible = false;
+
+Console.WriteLine("╔══════════════════╗");
+Console.WriteLine("║    LIVE CLOCK    ║");
+Console.WriteLine("╠══════════════════╣");
+Console.WriteLine("║     00:00:00     ║");
+Console.WriteLine("╚══════════════════╝");
+
+while (true)
+{
+    Console.SetCursorPosition(6, 3);
+    Console.Write(DateTime.Now.ToString("HH:mm:ss"));
+
+    Thread.Sleep(1000);
+}
+
